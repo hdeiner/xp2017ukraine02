5f28194a923ca23bb5424b2eff2bed98b7c5c4b5
Howard Deiner <howard.deiner@deinersoft.com>
2017-08-23 11:37:03 -0400

    Initial commit.

--- src/test/java/test/com/deinersoft/timeteller/TimeTellerTest.java
+++ src/test/java/test/com/deinersoft/timeteller/TimeTellerTest.java
@@ -27,21 +27,12 @@ public class TimeTellerTest {
 
     @Test
     public void gotLocalTimeCurrent(){
-        int localHour = LocalDateTime.now().getHour();
-        int localMinute = LocalDateTime.now().getMinute();
-        int localSecond = LocalDateTime.now().getSecond();
-        String localTimeNowFormatted = String.format("%02d:%02d:%02d", localHour, localMinute, localSecond);
-        assertThat(timeTeller.getResult(1,1,false), is(localTimeNowFormatted));
+        assertThat(timeTeller.getResult(1,1,false), is(getFormattedTime(LocalDateTime.now())));
     }
 
     @Test
     public void gotZuluTimeCurrent(){
-        LocalDateTime zuluTime = LocalDateTime.now(Clock.systemUTC());
-        int zuluHour = zuluTime.getHour();
-        int zuluMinute = zuluTime.getMinute();
-        int zuluSecond = zuluTime.getSecond();
-        String zuluTimeNowFormatted = String.format("%02d:%02d:%02dZ", zuluHour, zuluMinute, zuluSecond);
-        assertThat(timeTeller.getResult(2,1,false), is(zuluTimeNowFormatted));
+        assertThat(timeTeller.getResult(2,1,false), is(getFormattedTime(LocalDateTime.now(Clock.systemUTC()))+"Z"));
     }
 
     @Test
@@ -56,55 +47,63 @@ public class TimeTellerTest {
 
     @Test
     public void gotEmailForLocalTime(){
-        int localHour = LocalDateTime.now().getHour();
-        int localMinute = LocalDateTime.now().getMinute();
-        int localSecond = LocalDateTime.now().getSecond();
-        String localTimeNowFormatted = String.format("%02d:%02d:%02d", localHour, localMinute, localSecond);
+        String localTimeNowFormatted = getFormattedTime(LocalDateTime.now());
         timeTeller.getResult(1,1,true);
 
         boolean receivedEmail = false;
         for (int readAttempts = 1; (readAttempts <= 5) && (!receivedEmail); readAttempts++ ) {
-            IMAPFolder folder = null;
-            Store store = null;
-            String subject = null;
-            Flag flag = null;
-            try {
-                Properties props = System.getProperties();
-                props.setProperty("mail.store.protocol", "imaps");
+            receivedEmail = lookForTimeTellerEmail(localTimeNowFormatted);
+        }
+        assertThat(receivedEmail, is(true));
+    }
 
-                Session session = Session.getDefaultInstance(props, null);
-                store = session.getStore("imaps");
-                store.connect("imap.googlemail.com","howarddeiner.xyzzy@gmail.com", "birneraccount");
+    private String getFormattedTime(LocalDateTime clock){
+        int localHour = clock.getHour();
+        int localMinute = clock.getMinute();
+        int localSecond = clock.getSecond();
+        return String.format("%02d:%02d:%02d", localHour, localMinute, localSecond);
+    }
 
-                folder = (IMAPFolder) store.getFolder("inbox");
-                if(!folder.isOpen()) {
-                    folder.open(Folder.READ_WRITE);
-                    Message[] messages = folder.getMessages();
-                    for (int i=0; i < messages.length; i++) {
-                        Message msg = messages[i];
-                        if (msg.getSubject().equals("TimeTeller")) {
-                            if (((String)msg.getContent()).contains(localTimeNowFormatted)){
-                                receivedEmail = true;
-                                msg.setFlag(Flags.Flag.DELETED, true);
-                            }
+    private boolean lookForTimeTellerEmail(String localTimeNowFormatted){
+        boolean receivedEmail = false;
+        IMAPFolder folder = null;
+        Store store = null;
+        try {
+            Properties props = System.getProperties();
+            props.setProperty("mail.store.protocol", "imaps");
+
+            Session session = Session.getDefaultInstance(props, null);
+            store = session.getStore("imaps");
+            store.connect("imap.googlemail.com","howarddeiner.xyzzy@gmail.com", "birneraccount");
+
+            folder = (IMAPFolder) store.getFolder("inbox");
+            if(!folder.isOpen()) {
+                folder.open(Folder.READ_WRITE);
+                Message[] messages = folder.getMessages();
+                for (Message msg : messages) {
+                    if (msg.getSubject().equals("TimeTeller")) {
+                        if (((String) msg.getContent()).contains(localTimeNowFormatted)) {
+                            receivedEmail = true;
+                            msg.setFlag(Flag.DELETED, true);
                         }
                     }
                 }
             }
+        }
+        catch (Exception e) { }
+        finally {
+            try {
+                if (folder != null && folder.isOpen()) folder.close(true);
+                if (store != null) store.close();
+            }
             catch (Exception e) { }
-            finally {
-                try {
-                    if (folder != null && folder.isOpen()) folder.close(true);
-                    if (store != null) store.close();
-                }
-                catch (Exception e) { }
-            }
-            if (!receivedEmail) {
-                try { TimeUnit.SECONDS.sleep(1); }
-                catch(InterruptedException e){ }
-            }
         }
 
-        assertThat(receivedEmail, is(true));
+        if (!receivedEmail) {
+            try { TimeUnit.SECONDS.sleep(1); }
+            catch(InterruptedException e){ }
+        }
+
+        return receivedEmail;
     }
 }
